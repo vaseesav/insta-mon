2d5cbf95518e8d84290ae717f9386665afce8209
vaseesav <byvaste@gmail.com>
2023-04-21 20:42:20 +0200

    Added class DbInserter.

---
+++ database/db_inserter.py
@@ -0,0 +1,15 @@
+class DbInserter:
+    def __init__(self):
+        pass
+
+    def insert_target_table(self):
+        """
+        Function that inserts the target data into the target table.
+        """
+        pass
+
+    def insert_posts_table(self):
+        """
+        Function that inserts the posts data into the target table.
+        """
+        pass
