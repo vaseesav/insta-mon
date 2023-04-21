63b5463c104014fdf7e1949387fa971cefce8185
vaseesav <byvaste@gmail.com>
2023-04-21 20:39:19 +0200

    Added target_username var.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from database.db_creator import DbCreator
+from input.data_input import DataInput
 
 
 class InstaMon:
     def __init__(self):
-        self.target_name =
+        self.target_name = DataInput().get_target_username()
         self.db_name = self.target_name + "-data.db"
         self.db_creator = DbCreator(self.db_name)
 
