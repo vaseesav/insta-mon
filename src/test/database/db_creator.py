2ad9dc21db56027f90be2f4ece091e5499097463
VaseesaV <byvaste@gmail.com>
2023-04-24 21:39:45 +0200

    Bug fixes for several problems during the database requests.

--- src/test/database/db_creator.py
+++ src/test/database/db_creator.py
@@ -1,13 +1,12 @@
 import sqlite3
-from src.test.logger.logger_handler import LoggingHandler
-from src.test.database.db_handler import DbHandler
+
+from database.db_handler import DbHandler
 
 
 class DbCreator:
     def __init__(self, db_name):
         self.db_name = db_name
         self.db_handler = DbHandler(db_name)
-        self.logger = LoggingHandler(self.__class__.__name__).logger
 
     def create_tables(self):
         """
@@ -28,15 +27,12 @@ class DbCreator:
         try:
             cursor.execute(query, (table_name,))
             if cursor.fetchone() is not None:
-                self.logger.info("Table {} exists.".format(table_name))
                 return True
         except sqlite3.Error as se:
             print("An error occurred while checking for tables.", se)
-            self.logger.warning('An error occurred while checking for tables.', se)
             quit(-1)
         except Exception as e:
             print("An error occurred while checking for tables.", e)
-            self.logger.warning('An error occurred while checking for tables.', e)
             quit(-1)
 
     def create_target_table(self):
@@ -44,12 +40,11 @@ class DbCreator:
         Function which creates the target table.
         """
         try:
-            cursor = self.db_handler.cursor
             if self.table_exists("target"):
                 pass
             else:
-                self.logger.info("Creating target table.")
-                cursor.execute('''
+                cur = self.db_handler.cursor
+                cur.execute('''
                 CREATE TABLE target (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL,
@@ -59,18 +54,14 @@ class DbCreator:
                     post_amount INTEGER,
                     follower INTEGER,
                     followings INTEGER,
-                    profile_picture BLOB,
-                    profile_picture_path TEXT,
                     unix_time INTEGER NOT NULL
                 );
                 ''')
         except sqlite3.Error as se:
             print("An error occurred while creating the target table.", se)
-            self.logger.critical('An error occurred while creating the target table.', se)
             quit(-1)
         except Exception as e:
             print("An error occurred while creating the target table.", e)
-            self.logger.critical('An error occurred while creating the target table.', e)
             quit(-1)
         finally:
             self.db_handler.connection.commit()
@@ -81,7 +72,6 @@ class DbCreator:
             if self.table_exists("posts"):
                 pass
             else:
-                self.logger.info("Creating posts table.")
                 cursor.execute('''
                 CREATE TABLE posts (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -100,11 +90,9 @@ class DbCreator:
                 ''')
         except sqlite3.Error as se:
             print("An error occurred while creating the posts table.", se)
-            self.logger.critical('An error occurred while creating the posts table.', se)
             quit(-1)
         except Exception as e:
             print("An error occurred while creating the posts table.", e)
-            self.logger.critical('An error occurred while creating the posts table.', e)
             quit(-1)
         finally:
             self.db_handler.connection.commit()
