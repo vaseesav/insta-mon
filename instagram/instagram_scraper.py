61bd4fa212ccc5de0047f8973c89d477a8887fb9
VaseesaV <byvaste@gmail.com>
2023-04-24 21:39:45 +0200

    Bug fixes for several problems during the database requests.

--- instagram/instagram_scraper.py
+++ instagram/instagram_scraper.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from instagram.instagram_handler import InstagramHandler
 
 
 class InstagramScraper:
-    def __int__(self, target_name):
+    def __init__(self, target_name):
         self.target_name = target_name
-        self.instagram_handler = InstagramHandler()
+        self.instagram_handler = InstagramHandler(target_name)
 
     def get_metadata(self):
         """
@@ -19,10 +20,39 @@ class InstagramScraper:
             quit(-1)
 
     def get_profile_picture_url(self):
-        pass
+        """
+        Function that scraps the profile picture url of a user.
+        :return: profile_picture_url
+        """
+        try:
+            profile_picture_url = self.instagram_handler.profile.get_profile_pic_url()
+            return profile_picture_url
+        except Exception as e:
+            print("An error occurred while getting the profile picture url.", e)
+            quit(-1)
 
     def get_profile_picture(self):
-        pass
+        """
+        Function that downloads and saves the profile picture of a user.
+        """
+        try:
+            profile = self.instagram_handler.profile
+            picture_url = self.get_profile_picture_url()
+            target_name = self.target_name
+            path = Path(target_name + '/profile_pictures')
+            self.instagram_handler.bot.download_title_pic(picture_url, path, 'profile_picture', profile)
+        except Exception as e:
+            print("An error occurred while downloading or saving the profile picture.", e)
+            quit(-1)
 
     def get_posts(self):
-        pass
+        """
+        Function that scraps the posts of a user.
+        :return: posts
+        """
+        try:
+            posts = self.instagram_handler.profile.get_posts()
+            return posts
+        except Exception as e:
+            print("An error occurred while getting posts.", e)
+            quit(-1)
