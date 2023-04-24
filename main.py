fc8c347b9da1e3b2cf14b4ed1dc3944102c56d63
VaseesaV <byvaste@gmail.com>
2023-04-24 17:23:24 +0200

    Added handler for instagram scraping operations. Using the instaloader library.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from database.db_creator import DbCreator
 from database.db_inserter import DbInserter
 from input.data_input import DataInput
 from instagram.instagram_scraper import InstagramScraper
-from instagram.instagram_data_sorter import DataSorter
 
 
 class InstaMon:
@@ -11,11 +10,7 @@ class InstaMon:
         self.db_name = self.target_name + "-data.db"
         self.db_creator = DbCreator(self.db_name)
         self.instagram_scraper = InstagramScraper(self.target_name)
-        self.meta_data = self.instagram_scraper.get_metadata()
-        self.user_posts_data = self.instagram_scraper.get_posts()
-        self.data_sorter = DataSorter(self.meta_data, self.user_posts_data)
-        self.meta_data_sorted = self.data_sorter.get_metadata()
-        self.db_inserter = DbInserter(self.db_name, self.meta_data_sorted)
+        self.db_inserter = DbInserter(self.db_name, meta_data_sorted)
 
     def main(self):
         # Create database and its tables
