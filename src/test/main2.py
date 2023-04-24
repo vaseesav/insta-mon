f00edb64380215ff2fef80d5d572a10c59c3e0ff
VaseesaV <byvaste@gmail.com>
2023-04-24 17:52:12 +0200

    Added instagram_data_sorter. Sorts the data from instagram_scraper into a db_inserter suitable format.

--- src/test/main2.py
+++ src/test/main2.py
@@ -7,17 +7,14 @@ from instagram.instagram_data_sorter import DataSorter
 
 class InstaMon:
     def __init__(self):
-        self.target_name = DataInput().get_target_username_args()
-        self.login_data = DataInput().get_login_data()
+        self.target_name = DataInput().get_target_username()
         self.db_name = self.target_name + "-data.db"
         self.db_creator = DbCreator(self.db_name)
-        self.instagram_scraper = InstagramScraper(self.target_name, self.login_data)
+        self.instagram_scraper = InstagramScraper(self.target_name)
         self.meta_data = self.instagram_scraper.get_metadata()
-        self.image_name = ?
-        self.user_posts_data = self.instagram_scraper.get_posts()
-        self.data_sorter = DataSorter(self.meta_data, self.user_posts_data)
+        self.data_sorter = DataSorter(self.meta_data)
         self.meta_data_sorted = self.data_sorter.get_metadata()
-        self.db_inserter = DbInserter(self.db_name, self.meta_data_sorted, self.image_name)
+        self.db_inserter = DbInserter(self.db_name, self.meta_data_sorted)
 
     def main(self):
         # Create database and its tables
