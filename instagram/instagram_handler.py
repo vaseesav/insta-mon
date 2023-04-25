6f51e2024084f3b08213025a7dd39a6ec6567bd5
vaseesav <byvaste@gmail.com>
2023-04-25 15:27:21 +0200

    Updated exception handling for instagram_handler.

--- instagram/instagram_handler.py
+++ instagram/instagram_handler.py
@@ -4,15 +4,14 @@ from logger.logger_handler import LoggingHandler
 
 class InstagramHandler:
     def __init__(self, target_name):
-        self.logger = LoggingHandler(self.__class__.__name__).logger
-
         try:
-            self.logger.info("Logging in to Instagram.")
+            self.logger = LoggingHandler(self.__class__.__name__).logger
+            self.logger.info("Loging in to Instagram.")
             self.bot = instaloader.Instaloader()
             self.profile = instaloader.Profile.from_username(self.bot.context, target_name)
         except instaloader.LoginRequiredException as lre:
             print("Please login to your Instagram account. Or wait a few minutes and try again.", lre)
-            self.logger.critical("Failed to reach account page. Please login!", lre)
+            self.logger.critical("Failed to reach account page. Please login!")
             quit(-1)
         except Exception as e:
             print("An error occurred while communicating with the Instagram API.", e)
