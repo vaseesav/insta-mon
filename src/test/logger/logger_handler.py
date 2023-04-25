4a1ec0688771e5a71bb59421811ae0d20c762f7f
vaseesav <byvaste@gmail.com>
2023-04-25 14:04:14 +0200

    logger_handler changed so, that the actual class is written inside the logs.

--- src/test/logger/logger_handler.py
+++ src/test/logger/logger_handler.py
@@ -4,7 +4,7 @@ import logging
 class LoggingHandler:
     def __init__(self, cls_name):
         self.logger = logging.getLogger(cls_name)
-        self.handler = logging.FileHandler('login_and_data_fetcher.py-mon.log')
+        self.handler = logging.FileHandler('insta-mon.log')
         self.logger.setLevel(logging.DEBUG)
         self.format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.handler.setFormatter(self.format)
