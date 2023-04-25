1cab8674cfd1f99a2f58678007afac58114cc3f6
vaseesav <byvaste@gmail.com>
2023-04-25 15:00:39 +0200

    Added new Exception for too many requests.

--- instagram/instagram_handler.py
+++ instagram/instagram_handler.py
@@ -3,6 +3,13 @@ from instaloader import instaloader
 
 class InstagramHandler:
     def __init__(self, target_name):
-        self.bot = instaloader.Instaloader()
-        self.profile = instaloader.Profile.from_username(self.bot.context, target_name)
+        try:
+            self.bot = instaloader.Instaloader()
+            self.profile = instaloader.Profile.from_username(self.bot.context, target_name)
+        except instaloader.LoginRequiredException as lre:
+            print("Please login to your Instagram account. Or wait a few minutes and try again.", lre)
+            quit(-1)
+        except Exception as e:
+            print("An error occurred while communicating with the Instagram API.", e)
+            quit(-1)
         
