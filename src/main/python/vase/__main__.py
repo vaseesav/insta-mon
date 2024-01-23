cf1f9ecd997532534a5897fe04c7e94ef122a214
vaseesav <byvaste@gmail.com>
2024-01-23 21:32:14 +0100

    Initial commit. Rebasing the whole script.

---
+++ src/main/python/vase/__main__.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+"""
+Insta-mon: A tool to monitor someone's Instagram account
+
+This module contains the main entry point for the application.
+"""
+
+import sys
+
+
+if __name__ == '__main__':
+    # Check if the correct python version is being used
+    python_version = sys.version.split()[0]
+
+    if sys.version_info < (3, 5):
+        print('The minimum required Python version is 3.5+ \n'
+              'The version you are using is %s' % python_version)
+        quit(-1)
+
+    import instamon
+    instamon.main()
--- src/test2/main.py
+++ src/test2/main.py
@@ -1,18 +1,14 @@
-import input.cli_data_input
-import db.target_data_db_operations
-
-
 class InstaMon:
     def __init__(self):
         # Parsing the start arguments
-        cli_data_input = input.cli_data_input.DataInput()
+        cli_data_input = src.test2.input.cli_data_input.DataInput()
         cli_data_input.data_input()
         self.target_id = cli_data_input.get_target_id()
         self.login_data = cli_data_input.get_login_data()
         self.stop_date = cli_data_input.get_stop_date()
 
         # Parsing the arguments to the db
-        target_data_db_operations = db.target_data_db_operations.CreateTargetDatabase(str(self.target_id))
+        target_data_db_operations = src.test2.db.target_data_db_operations.CreateTargetDatabase(str(self.target_id))
         target_data_db_operations.create_tables()
 
     def main(self):
