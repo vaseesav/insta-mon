0844c80e1b7fa4fd386221273a47d9eba0c5b2b8
vaseesav <byvaste@gmail.com>
2024-01-25 22:05:29 +0100

    Changed the login_and_data_fetcher.py to be more efficient

--- src/main/python/vase/login_and_data_fetcher.py
+++ src/main/python/vase/login_and_data_fetcher.py
@@ -1,30 +1,53 @@
-"""Insta-mon Insta module
+"""Insta-mon Insta module with login functionality."""
 
-This module handles the Instagram login process,
-retrieves data from the user's profile, and transforms it into a structured format.
-"""
+import logging
+from typing import Optional
 
 from instagrapi import Client
 
 
-class InstagramLogin:
-    """
-    Class that handles the Insta login process
-    """
+class InstaLogin:
+    """Handles the Insta login process."""
 
-    def __init__(self):
-        self.client = None
-        self.username = None
-        self.password = None
+    def __init__(self) -> None:
+        self.client: Client = None
 
-    def login(self, username, password, verification_code=None):
-        self.client = Client()
+        # logging configuration
+        logging.basicConfig(
+            filename='insta_login.log',
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s"
+        )
 
-        if verification_code is not None:
-            self.client.login(username, password, verification_code=verification_code)
-        else:
-            self.client.login(username, password)
+    def perform_login(self, username: str, password: str, verification_code: Optional[str] = None) -> bool:
+        """
+        Attempts to log in to Insta using provided credentials.
 
-        self.username = username
-        self.password = password
+        Args:
+            username: Insta username.
+            password: Insta password
+            verification_code: Verification code if two-factor authentication is enabled
 
+        Returns:
+            True if login is successful, False otherwise.
+        """
+
+        try:
+            if not self.client:
+                self.client = Client()
+
+            if verification_code:
+                self.client.login(username, password, verification_code=verification_code)
+            else:
+                self.client.login(username, password)
+
+            logging.info("Login successful for user: %s", username)
+            return True
+
+        except self.client.handle_exception as e:
+            logging.error("Login failed: %s", str(e))
+            print("Login failed. Please check your credentials and try again.", e)
+            return False
+        except Exception as e:
+            logging.error("Login failed: %s", str(e))
+            print("Login failed. Please check your credentials and try again.", e)
