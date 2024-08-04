a3483d968accd72bb1bf5443f07a6a17f1e13d05
vaseesav <byvaste@gmail.com>
2024-08-04 20:05:23 +0200

    added post download for images and videos (only single file posts)

--- src/main.py
+++ src/main.py
@@ -12,9 +12,10 @@ import requests
 from PIL import Image
 from instagrapi import Client
 
+import src
 from src import config, log
-from src.data_models import User
-from src.db import insert_user, get_last_user_info
+from src.data_models import User, ImagePost, ReelPost
+from src.db import insert_user, get_last_user_info, insert_post, get_last_post_info
 
 # Setup logging
 log.setup_logging()
@@ -90,9 +91,58 @@ def download_image(url: str, target_type: TargetType) -> str:
     return file_path
 
 
+def download_video(url: str) -> str:
+    """
+    Downloads a video from the provided URL and saves it to the 'target/posts' directory.
+    If the video already exists at the target location, it will not be downloaded again.
+
+    :param url: The URL of the video to be downloaded.
+
+    :return: The path where the video is saved or already exists.
+
+    :raises requests.exceptions.RequestException: If there is an issue with the HTTP request.
+    :raises OSError: If there is an issue creating directories or saving the file.
+    """
+
+    # Parse the URL to extract the file name
+    parsed_url = urlparse(url)
+    file_name = os.path.basename(parsed_url.path)
+
+    # Set the target directory
+    target_dir = os.path.join('target', 'posts')
+    os.makedirs(target_dir, exist_ok=True)
+
+    # Full path to save the video
+    file_path = os.path.join(target_dir, file_name)
+
+    # Check if the file already exists
+    if os.path.exists(file_path):
+        logger.debug("Video already exists at %s. Skipping download.", file_path)
+        return file_path
+
+    try:
+        # Send HTTP request to download the video
+        response = requests.get(url, timeout=30, stream=True)
+        response.raise_for_status()
+
+        # Save the video in chunks to handle large files
+        with open(file_path, 'wb') as video_file:
+            for chunk in response.iter_content(chunk_size=1024 * 1024):  # 1 MB chunks
+                video_file.write(chunk)
+        logger.debug("Video successfully downloaded and saved to %s", file_path)
+    except requests.exceptions.RequestException as request_error:
+        logger.error("Failed to download video from url: %s with error: %s", url, request_error)
+        raise
+    except OSError as os_error:
+        logger.error("Failed to save video from url: %s with error: %s", url, os_error)
+        raise
+
+    return file_path
+
+
 def create_user_obj(target_user: instagrapi.types.User) -> User:
     """
-    Function to create an instagram user object.
+    Function to create an insta-mon user object.
 
     :param target_user: user scraped from instagram
 
@@ -120,6 +170,43 @@ def create_user_obj(target_user: instagrapi.types.User) -> User:
     return parsed_target_user
 
 
+def create_post_obj(medias: instagrapi.types.Media) -> object:
+    """
+    Function to create an insta-mon Post object.
+
+    :param medias: media scraped from instagram
+
+    :return: post object for db
+    """
+    for media in medias:
+        product_type = media.product_type
+        post_id = media.id
+        description = media.caption_text
+        like_amount = media.like_count
+        comment_amount = media.comment_count
+
+        if product_type is None or product_type == '':
+            image_path = download_image(str(media.thumbnail_url), TargetType.POST)
+            parsed_media = ImagePost(
+                post_id=post_id,
+                picture_path=image_path,
+                description=description,
+                like_amount=like_amount,
+                comment_amount=comment_amount
+            )
+        elif product_type in ("igtv", "feed"):
+            video_path = download_video(str(media.video_url))
+            parsed_media = ReelPost(
+                post_id=post_id,
+                video_path=video_path,
+                description=description,
+                like_amount=like_amount,
+                comment_amount=comment_amount
+            )
+
+    return parsed_media
+
+
 def users_are_different(user1: User, user2: User) -> bool:
     """
     Compare two user objects and return True if they are different in any way.
@@ -129,6 +216,17 @@ def users_are_different(user1: User, user2: User) -> bool:
     :return: Boolean indicating if the users are different
     """
     return user1.__dict__ != user2.__dict__
+
+
+def posts_are_different(post1: src.data_models.post.Post, post2: src.data_models.post.Post) -> bool:
+    """
+    Compare two user objects and return True if they are different in any way.
+
+    :param post1: First post object
+    :param post2: Second post object
+    :return: Boolean indicating if the posts are different
+    """
+    return post1.__dict__ != post2.__dict__
 
 
 class InstaMon:
@@ -160,6 +258,29 @@ class InstaMon:
 
         if last_user is None or users_are_different(new_target_user, last_user):
             insert_user(new_target_user)
+
+    def get_media_data(self, target_username: str) -> object:
+        """
+        Function to get media data from instagram for a certain username.
+
+        :param target_username: instagram username
+        :return: instagram user media data
+        """
+        user_id = self.client.user_id_from_username(username=target_username)
+        return self.client.user_medias(user_id=user_id, amount=1)
+
+    def handle_user_media_data(self, target_username: str) -> None:
+        """
+        Function which handles the media data
+
+        :param target_username: instagram username
+        """
+        media = self.get_media_data(target_username=target_username)
+        post = create_post_obj(media)
+        last_post = get_last_post_info()
+
+        if last_post is None or posts_are_different(post, last_post):
+            insert_post(post)
 
     def insta_scrap_query_handler(self) -> None:
         """
@@ -194,6 +315,7 @@ class InstaMon:
                 time.sleep(config.get("SCRAP_INTERVAL"))
             first_run = False
             self.handle_user_data(target_username=TARGET_USERNAME)
+            self.handle_user_media_data(target_username=TARGET_USERNAME)
             logger.info('Run completed!')
 
 
